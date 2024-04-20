9116648d9ac16efb2813f51b5894f0f190977a2b
Giovanni Useche <giovanni.useche@hotmail.com>
2024-04-19 21:52:02 -0400

    Update content

--- js/script.js
+++ js/script.js
@@ -10,6 +10,7 @@ const geographyPopulation = document.getElementById('geography-population');
 const languagesCurrency = document.getElementById('languages-currency');
 const additionalInformation = document.getElementById('additional-information');
 const imgFlags = document.getElementById('img-flags');
+const maps = document.getElementById('maps');
 
 /* 
 First I use the "fetch" to get the data from the REST Countries API
@@ -28,44 +29,44 @@ fetch('https://restcountries.com/v3.1/all')
 
     // Add an "ADDEventListener" to know which country the user selected
     countrySelect.addEventListener('change', function() {
-      const selectedCountryName = this.value;
-      const selectedCountry = data.find(country => country.name.common === selectedCountryName);
+        const selectedCountryName = this.value;
+        const selectedCountry = data.find(country => country.name.common === selectedCountryName);
 
-      // I show the results in the HTML of the selected country
-      imgFlags.innerHTML = `
-        <img src="${selectedCountry.flags.svg}" alt="${selectedCountry.flags.alt}">
-      `;
-      generalInformation.innerHTML = `
-        <h1>General Information</h1>
-        <hr>
-        <h1>${selectedCountry.name.common}</h1>
-        <p>Official Name: ${selectedCountry.name.official}</p>
-      `;
-      geographyPopulation.innerHTML = `
-        <h1>Geography and Population</h1>
-        <hr>
-        <p>Capital: ${selectedCountry.capital[0]}</p>
-        <p>Region: ${selectedCountry.region}</p>
-        <p>Subregion: ${selectedCountry.subregion}</p>
-        <p>Population: ${selectedCountry.population}</p>
-        <p>Fifa: ${selectedCountry.fifa[0]}</p>
-        <p>Continents: ${selectedCountry.continents}</p>
-        <p>Area: ${selectedCountry.area}</p>
-      `;
-      languagesCurrency.innerHTML = `
-        <h1>Languages and Currency</h1>
-        <hr>
-        <p>Official Language: ${Object.values(selectedCountry.languages).join(', ')}</p>
-        <p>Currency: ${Object.values(selectedCountry.currencies).map(currency => `${currency.name}`).join(', ')}</p>
-        <p>Currency Symbol: ${Object.values(selectedCountry.currencies).map(currency => `(${currency.symbol})`).join(', ')}</p>
-      `;
-      additionalInformation.innerHTML = `
-        <h1>Additional Information</h1>
-        <hr>
-        <p>Independent: ${selectedCountry.independent}</p>
-        <p>Timezones: ${selectedCountry.timezones[0]}</p>
-        <p>Start Of Week: ${selectedCountry.startOfWeek}</p>
-        <p>Spellings: ${selectedCountry.altSpellings[1]}</p>
-      `;
+        // I show the results in the HTML of the selected country
+        imgFlags.innerHTML = `
+            <img src="${selectedCountry.flags.svg}" alt="${selectedCountry.flags.alt}">
+        `;
+        generalInformation.innerHTML = `
+            <h1>General Information</h1>
+            <hr>
+            <h1>${selectedCountry.name.common}</h1>
+            <p>Official Name: ${selectedCountry.name.official}</p>
+        `;
+        geographyPopulation.innerHTML = `
+            <h1>Geography and Population</h1>
+            <hr>
+            <p>Capital: ${selectedCountry.capital[0]}</p>
+            <p>Region: ${selectedCountry.region}</p>
+            <p>Subregion: ${selectedCountry.subregion}</p>
+            <p>Population: ${selectedCountry.population}</p>
+            <p>Fifa: ${selectedCountry.fifa[0]}</p>
+            <p>Continents: ${selectedCountry.continents}</p>
+            <p>Area: ${selectedCountry.area}</p>
+        `;
+        languagesCurrency.innerHTML = `
+            <h1>Languages and Currency</h1>
+            <hr>
+            <p>Official Language: ${Object.values(selectedCountry.languages).join(', ')}</p>
+            <p>Currency: ${Object.values(selectedCountry.currencies).map(currency => `${currency.name}`).join(', ')}</p>
+            <p>Currency Symbol: ${Object.values(selectedCountry.currencies).map(currency => `(${currency.symbol})`).join(', ')}</p>
+        `;
+        additionalInformation.innerHTML = `
+            <h1>Additional Information</h1>
+            <hr>
+            <p>Independent: ${selectedCountry.independent}</p>
+            <p>Timezones: ${selectedCountry.timezones[0]}</p>
+            <p>Start Of Week: ${selectedCountry.startOfWeek}</p>
+            <p>Spellings: ${selectedCountry.altSpellings[1]}</p>
+        `;  
     });
 })
